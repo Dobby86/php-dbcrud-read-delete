1721bb08016551448bd208d7dcec0ff8ca145835
marco <dobermannpoker86@gmail.com>
2020-05-29 18:01:26 +0200

    ++css

--- index.php
+++ index.php
@@ -7,21 +7,21 @@ header('Content-Type:  application/json');
     $password = 'root';
     $dbName = 'newhoteltest';
 
-    //fornire i 4 dati che ha il phpmyadmin
+    //fornire i 4 dati che ha il phpmyadmin e  connettersi a questi(scrivere nello stesso ordine)
 
     $conn = new mysqli($server, $username, $password, $dbName);
-
+//controllino
     if ($conn -> connect_errno) {
 
-        echo ($conn -> connect_errno);
+        echo $Sconn -> connect_errno;
         return;
     }
-
+//generiamo con un nome a caso(sql)
     $sql = "
        SELECT  id, name, lastname
        FROM paganti
         " ;
-
+//facciamo una query alla connesione, una sorte di funzione che passiamo l argomento sql
     $results = $conn -> query($sql);
 
     if   ($results -> num_rows < 1) {
@@ -29,7 +29,7 @@ header('Content-Type:  application/json');
           return;
         }
         $res = [ ];
-
+//fa il giro per le righe,e restituisce un NUN o Null e si blocca il while(mette a &row tutte le righe risultanti della query )
         while ($row = $results -> fetch_assoc()) {
 
             $res[ ] =  $row ;
